16141b8515499a04fd158ebc7870b8262e83935d
Diego Osorio <dieferosorio@utp.edu.co>
2020-08-02 17:47:49 -0500

    2020/08/02 modulo:material

---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+
+        MatToolbarModule,
+        MatCardModule,
+        MatListModule
+    ],
+    exports: [
+        MatToolbarModule,
+        MatCardModule,
+        MatListModule
+    ]
+})
+export class MaterialModule { }
